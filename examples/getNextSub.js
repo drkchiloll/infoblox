a69d0f8dee9a5725fd3575952b84476ed235947c
drkchiloll <samwomack_@me.com>
2016-10-07 07:38:06 -0500

    added example for getting next available subnet

--- examples/getNextSub.js
+++ examples/getNextSub.js
@@ -1,20 +1,20 @@
-var Infoblox = require('../lib/infoblox'),
-    config = require('./config');
+// var Infoblox = require('../lib/infoblox'),
+//     config = require('./config');
 
-var ipam = new Infoblox({
-  ip: config.ip,
-  apiVersion: config.apiVersion
-});
+let  Infoblox = require('../lib/infoblox'),
+     { ip, apiVersion, user, password } = require('./config');
 
-ipam.login(config.user, config.password).then(() => {
+var ipam = new Infoblox({ ip, apiVersion });
+
+ipam.login(user, password).then(() => {
   return;
 }).then(() => {
   return ipam.getContainer('10.255.0.0/16');
 }).then((data) => {
   let netId = JSON.parse(data)[0]._ref;
   return ipam.getNextSubnets({
-    netId, cidr: 21, num: 3
+    netId, cidr: 22, num: 1
   })
 }).then(data => {
-  // console.log(data);
+  console.log(data);
 }).catch(err => console.log(err))
